64056017d64a13e003d44814418ade346eba8d96
ganping <2669453205@qq.com>
2020-06-24 10:59:03 +0800

    修改克隆的

--- git-test/src/cn/itcast/git/GitTest.java
+++ git-test/src/cn/itcast/git/GitTest.java
@@ -3,9 +3,11 @@ package cn.itcast.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        //我丢
     }
     public void test(){
         int i=0;
         i+=10 ;
+        System.out.println(i);
     }
 }
